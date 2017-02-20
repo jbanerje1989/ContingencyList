dda0f73d5ed84a6db4b04fde5de98a90a48b9b6c
Joydeep <jbanerje@asu.edu>
2017-02-20 12:08:02 -0700

    Test

--- InterdependentNetwork/src/PowerNetworkDependency/IIRGenerator.java
+++ InterdependentNetwork/src/PowerNetworkDependency/IIRGenerator.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 // Refer the corresponding MATLAB file for format of the data retrieved
-// Author: Joydeep Banerje
+// Author: Joydeep Banerjee
 
 public class IIRGenerator {
 	
